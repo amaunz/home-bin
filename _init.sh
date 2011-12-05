24d81a2b93e473a073e407dc2fb40c5e8cc375ff
ot1 <ot1@toxcreate3.in-silico.ch>
2011-12-05 16:18:45 +0100

    Fixed git wrapper

--- _init.sh
+++ _init.sh
@@ -2,4 +2,6 @@
 # Copies bash, git, vim and irb init files to $HOME
 # WARNING: Backups will be made, but of course user-defined settings in the files get lost.
 
-cp -v --backup=numbered .bashrc .bash_aliases .vimrc .irbrc .gitconfig "$HOME"
+cp -v --backup=numbered .gitconfig_cfg .gitconfig 
+sed -i "s,REPO_DIR,`pwd`,g" .gitconfig
+#cp -v --backup=numbered .bashrc .bash_aliases .vimrc .irbrc .gitconfig "$HOME"
