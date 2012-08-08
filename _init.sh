a56b086cf39e9c4f62f9b8c1e543ad33820d68b9
Andreas Maunz <andreas@maunz.de>
2012-08-08 14:31:59 +0200

    Force backup before vimdiff

--- _init.sh
+++ _init.sh
@@ -11,7 +11,7 @@ sed -i "s,REPO_DIR,`pwd`,g" .gitconfig
 
 for f in .bashrc .bash_aliases .vimrc .gvimrc .irbrc .gitconfig .screenrc .htoprc .lessfilter; do 
   if [ -f "$HOME/$f" ] && ! cmp "$f" "$HOME/$f" >/dev/null 2>1; then
-    read -p "File  $HOME/$f exists. Vimdiff (y/N)? " yn && case $yn in [Yy]* ) vimdiff "$f" "$HOME/$f" ; esac
+    read -p "File  $HOME/$f exists. Vimdiff (y/N)? " yn && case $yn in [Yy]* ) (cp -v --backup=numbered "$HOME/$f" "$HOME/$f~" && vimdiff "$f" "$HOME/$f") ; esac
   else
     cp -v $f "$HOME"
   fi
