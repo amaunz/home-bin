88d65923659c690002220e2278a1def01619a62d
Andreas Maunz <andreas@maunz.de>
2012-09-15 16:27:31 +0200

    Rem

--- .conky/gmail.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo > /tmp/mails_full.txt
-atomfeed=`wget -q -O - https://mail.google.com/a/gmail.com/feed/atom --http-user=andreas@maunz.de --http-password="oEYkET298jxn6SJS" --no-check-certificate`
-count=`echo $atomfeed | sed 's,.*<fullcount>\(.*\)</fullcount>.*,\1,g'`
-title=`echo $atomfeed | sed 's,.*<title>\(.*\)</title>.*,\1,g'`
-name=`echo $atomfeed | sed 's,.*<name>\(.*\)</name>.*,\1,g'`
-echo "$count: $name: '$title'"
